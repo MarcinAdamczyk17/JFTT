49a93f9f4ffc82712b132167601c632836fc663a
MarcinAdamczyk17 <marcin8551@gmail.com>
2016-10-16 20:34:02 +0200

    file reading

--- DFA/src/main/Test.java
+++ DFA/src/main/Test.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		File input = new File("input.txt");
+		Scanner scanner = null;
+		
+		try{
+			scanner = new Scanner(input);
+		}
+		catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		
+		while(scanner.hasNextLine()){
+			System.out.println(scanner.nextLine());
+		}
 	}
 
 }
